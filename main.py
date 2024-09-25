f1786261a662263a6750975202b0fedc4118d165
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2024-09-25 10:12:58 -0400

    add the version bug too

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from typing import Optional
+
 import typer
 from typing_extensions import Annotated
 
 app = typer.Typer()
+
+__version__ = "0.1.0"
+
+
+def version_callback(value: bool):
+    if value:
+        print(f"Awesome CLI Version: {__version__}")
+        raise typer.Exit()
 
 
 def complete_name(incomplete: str):
@@ -12,11 +22,16 @@ def complete_name(incomplete: str):
     ]
     return [name for name in valid_names if name.startswith(incomplete)]
 
+
 @app.command()
-def main(name: Annotated[str, typer.Option(
-    help="The name to say hi to.",
-    autocompletion=complete_name
-)] = "World"):
+def main(
+    name: Annotated[
+        str, typer.Option(help="The name to say hi to.", autocompletion=complete_name)
+    ] = "World",
+    version: Annotated[
+        Optional[bool], typer.Option("--version", callback=version_callback)
+    ] = None,
+):
     print(f"Hello {name}")
 
 
